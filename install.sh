5e6c8dea6ae95107b36738e447f9f6e9289dbbfa
Kohar Jidan <kohar.jidan@gmail.com>
2021-09-12 10:41:43 +0700

    install script fix

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ echo "Install idx-tools to $HOME/bin"
 
 chmod +x R/idxDaily.R
 
+rm $HOME/bin/idxDaily
 ln -sf `pwd`/R/idxDaily.R $HOME/bin/idxDaily
+
+Rscript -e 'install.packages("optparse", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("quantmod", repos="http://cran.r-project.org")'
