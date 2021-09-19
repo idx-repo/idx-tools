5ff7966a8d3528961871c4d004c06de094895cdd
Karina Vega <akun.karina.vega@gmail.com>
2021-09-19 08:17:10 +0700

    install.sh [fix], remove.sh [add], update idx data [add]

--- install.sh
+++ install.sh
@@ -3,9 +3,22 @@
 echo "Install idx-tools to $HOME/bin"
 
 chmod +x R/idxDaily.R
+chmod +x Bash/update-data.sh
 
-rm $HOME/bin/idxDaily
 ln -sf `pwd`/R/idxDaily.R $HOME/bin/idxDaily
+ln -sf `pwd`/Bash/update-data.sh $HOME/bin/idxdata-update
 
-Rscript -e 'install.packages("optparse", repos="http://cran.r-project.org")'
-Rscript -e 'install.packages("quantmod", repos="http://cran.r-project.org")'
+if [ ! -d "$HOME/R/x86_64-suse-linux-gnu-library/4.1/optparse" ]
+then
+ Rscript -e 'install.packages("optparse", repos="http://cran.r-project.org")'
+fi
+if [ ! -d "$HOME/R/x86_64-suse-linux-gnu-library/4.1/quantmod" ]
+then
+ Rscript -e 'install.packages("quantmod", repos="http://cran.r-project.org")'
+fi
+if [ ! -d "$HOME/.idx-data" ]
+then
+ git clone https://github.com/idx-repo/idx-data.git ~/.idx-data
+else
+ idxdata-update
+fi
---
+++ remove.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Remove idx-tools to $HOME/bin"
+rm -rf $HOME/bin/idxDaily
+rm -rf $HOME/bin/idxdata-update
+rm -rf $HOME/.idx-data
