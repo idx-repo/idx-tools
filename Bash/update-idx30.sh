80c3144eaa2d5e1108465f447d468030e02b7ec0
Karina Vega <akun.karina.vega@gmail.com>
2023-02-08 13:04:24 +0700

    idx30data-update: fix

--- Bash/update-idx30.sh
+++ Bash/update-idx30.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(cut -d "," -f 1 $HOME/.idx-data/Index/IDX30.csv | tr -d '"' | tail -n +2)
+for i in $(cut -d "|" -f 1 $HOME/.idx-data/Index/IDX30.csv | tr -d '"' | tail -n +2)
 do
  echo "Update $i"
  idxDaily -e $i -o "$i.csv"
